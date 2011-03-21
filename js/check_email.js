da1dab3944bf300ca397dee4ef256fa9749a1e5f
hnJaermuseet <hn@jaermuseet.no>
2011-03-21 08:58:53 +0100

    Edit entry: JQuery validation of emailfield supports multiple emails. Closes #21

--- js/check_email.js
+++ js/check_email.js
@@ -65,18 +65,44 @@ function checkEmailAndAlert_editentry (field)
 	
 	checked = tr.find('input[type=checkbox]').attr('checked');
 	
-	if(!checkEmail (field.val()))
+	// Splitting field.val()
+	values = field.val().split(/[\s,;]+/);
+	invalid_emails = 0; valid_emails = 0;
+	for(i = 0; i < values.length; i++)
+	{
+		if(values[i] == '') {
+		
+		} else if(checkEmail (values[i])) {
+			valid_emails++;
+		} else {
+			invalid_emails++;
+		}
+	}
+	if(invalid_emails > 0)
 	{
 		// Invalid email
 		field.addClass('haserror');
 		tdmessage.children('.emailok').slideUp('slow');
+		
+		if(invalid_emails == 1 && valid_emails == 0)
+			emailmsg = 'Ugyldig e-post';
+		else if(invalid_emails == 1)
+			emailmsg = invalid_emails + ' ugyldig e-post';
+		else
+			emailmsg = invalid_emails + ' ugyldige e-poster';
+		if(valid_emails == 1)
+			emailmsg += '. ' + valid_emails + ' gyldig e-post.';
+		else if (valid_emails > 0)
+			emailmsg += '. ' + valid_emails + ' gyldige e-poster.';
+		
 		if(tdmessage.children('.emailerror').length == 0)
 		{
-			tdmessage.append('<div class="emailerror"><img src="img/icons/delete.png"> Ugyldig e-post</div>');
+			tdmessage.append('<div class="emailerror"><img src="img/icons/delete.png"><span class="emailmsg"> '+emailmsg+'</span></div>');
 		}
 		else
 		{
 			tdmessage.children('.emailerror').slideDown('slow');
+			tdmessage.children('.emailerror').children('.emailmsg').text(' ' + emailmsg);
 		}
 	}
 	else if(field.val() != '')
@@ -84,13 +110,20 @@ function checkEmailAndAlert_editentry (field)
 		// Valid email
 		field.removeClass('haserror');
 		tdmessage.children('.emailerror').slideUp('slow');
+		
+		if(valid_emails > 1)
+			emailmsg = ' ' + valid_emails + ' gyldige e-poster.';
+		else
+			emailmsg = '';
+		
 		if(tdmessage.children('.emailok').length == 0)
 		{
-			tdmessage.append('<div class="emailok"><img src="img/icons/accept.png"></div>');
+			tdmessage.append('<div class="emailok"><img src="img/icons/accept.png"><span class="emailmsg">'+emailmsg+'</span></div>');
 		}
 		else
 		{
 			tdmessage.children('.emailok').slideDown('slow');
+			tdmessage.children('.emailok').children('.emailmsg').text(emailmsg);
 		}
 	}
 	else
