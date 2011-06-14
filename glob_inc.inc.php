81ad6cddf6e9e33b65817fe1b7e093512315c519
hnJaermuseet <hn@jaermuseet.no>
2011-06-14 09:53:48 +0200

    Disabling debugging

--- glob_inc.inc.php
+++ glob_inc.inc.php
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
 $debug_log = '';
-$debug = true;
+$debug = false;
 if($debug)
 {
 	$debug_time_start = microtime(true);
